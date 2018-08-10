1ecb28b300158320d68ac9d8d5610a8f76b380b5
Matt Smith <mattsmithereens@gmail.com>
2018-08-10 12:12:59 -0700

    refine file structure, add main.js

--- src/js/BetterDoctorAPI.js
+++ src/js/BetterDoctorAPI.js
@@ -1,9 +1,11 @@
+//const apiKey = require('./../.env').apiKey;
+
 export class BetterDoctorAPI{
-  getLatLong(zip) {
+  //getLatLong(zip) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
       // hardcode in PDX OR
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=headache&location=or-portland&user_location=45.5122%2C-122.6587&sort=distance-asc&skip=0&limit=10&user_key=${process.env.apiKey}`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${symptom}&location=${location}&sort=best-match-asc&skip=0&limit=${perPage}&user_key=${apiKey}`;
 
       request.onload = function() {
         if (this.status === 200) {
