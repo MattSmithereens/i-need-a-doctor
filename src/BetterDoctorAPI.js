83269afa07e4c81efc4802e8778f9ff44ec0db90
Matt Smith <mattsmithereens@gmail.com>
2018-08-11 17:29:51 -0700

    wrapping it up for the evening.  API call works, but am having issues formatting and displaying the data

--- src/BetterDoctorAPI.js
+++ src/BetterDoctorAPI.js
@@ -3,7 +3,8 @@ export class BetterDoctorAPI{
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
       // hardcode in PDX OR... do GeocoderAPI if you have time
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${symptom}&location=or-portland&sort=best-match-asc&skip=0&limit=${perPage}&user_key=${process.env.exports.apiKey}`;
+      // why is this rendering as XML?  let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${symptom}&location=or-portland&sort=best-match-asc&skip=0&limit=${perPage}&user_key=${process.env.exports.apiKey}`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${symptom}&location=or-portland&sort=distance-asc&skip=0&limit=${perPage}&user_key=${process.env.exports.apiKey}`;
       // `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${symptom}&location=${location}&sort=best-match-asc&skip=0&limit=${perPage}&user_key=${process.env.API_KEY}`; if using geocoder
       console.log(url);
       request.onload = function() {
--- src/main.js
+++ src/main.js
@@ -17,36 +17,20 @@ $(document).ready(function() {
     let newSearch = betterDoctorAPI.getSearches(name, symptom, perPage);
     newSearch.then(function(response) {
       let body = JSON.parse(response);
+      console.log(body);
       if ( body.data.length === 0) {
         $("#result").append("No results found for that search.");
       } else {
 
         for (let i = 0; i < body.data.length; i++) {
-          $('#result').append(   '<h2>' + body.data[i].profile.first_name +
-                                ' ' + body.data[i].profile.last_name + '</h2>' +
-                                '<li>' + body.data[i].practices[0].visit_address.street + '</li>' +
-                                '<li>' + body.data[i].practices[0].visit_address.city + ', ' + body.data[i].practices[0].visit_address.state + " " + body.data[i].practices[0].visit_address.zip + '</li>' +
-                                '<li>' + body.data[i].specialties[0].name + '</li>' +
-                                '<li>' + '<b>' + 'Phone # : ' + '</b>' + body.data[i].practices[0].phones[0].number + "</li>"
+          $('#result').append(
+            '<p>${body.data[i].profile.first_name} ${body.data[i].profile.last_name}</p>'
           );
-          if (body.data[i].practices[0].website === undefined) {
-            $('#result').append('<li>' + "No website listed" + '</li>');
-          } else {
-            $('#result').append('<li>' + body.data[i].practices[0].website + '</li>');
-          }
-
-          if (body.data[i].practices[0].accepts_new_patients === true) {
-            $('#result').append('<li>' + "Accepting new patients." + '</li>');
-          } else if (body.data[i].practices[0].accepts_new_patients === false) {
-            $('#result').append('<li>' + "Currently not accepting new patients." + '</li>');
-          } else {
-            console.log("reached");
-          }
         }
       }
 
     }, function (error) {
-      $('.showErrors').text('There was an error: ${error.message}');
+      $('.showErrors').text('error: ${error.message}');
 
 
 
